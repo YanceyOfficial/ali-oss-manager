15a9a7e8c2f49ed32a7e781835637de273ba4a39
yanceyleo <developer@yanceyleo.com>
2019-06-17 16:44:18 +0800

    feat: Init repo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        我爱山本彩
+        <p>{process.title}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
